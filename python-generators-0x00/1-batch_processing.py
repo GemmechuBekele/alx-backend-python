3c44205db3d6969c58dfa10495eec298f13ea6a4
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-18 08:44:45 +0300

    Modify 1-batch_processing file

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,37 +1,36 @@
 import mysql.connector
-import os
 from dotenv import load_dotenv
-from typing import Generator, List, Dict
+import os
 
 load_dotenv()
 
-def stream_users_in_batches(batch_size: int) -> Generator[List[Dict], None, None]:
-    db_connection = mysql.connector.connect(
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME", "ALX_prodev")
     )
-
-    cursor = db_connection.cursor(dictionary=True)
+    cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
 
     batch = []
-    for row in cursor:
+    for row in cursor:  
         batch.append(row)
         if len(batch) == batch_size:
             yield batch
             batch = []
 
-    # Yield the last batch if not empty
     if batch:
         yield batch
 
     cursor.close()
-    db_connection.close()
+    connection.close()
 
-def batch_processing(batch_size: int) -> None:
-    for batch in stream_users_in_batches(batch_size):
-        for user in batch:
-            if user["age"] > 25:
-                print(user)
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):  
+        filtered_batch = (user for user in batch if user["age"] > 25)  
+        for user in filtered_batch:  
+            yield user
+
