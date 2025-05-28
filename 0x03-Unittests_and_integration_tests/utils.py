4fe9f00df02ea8bbd6a841e30a03c213077de646
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-28 18:51:42 +0300

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import requests
 """
 Utils module with access_nested_map
 """
@@ -8,3 +9,7 @@ def access_nested_map(nested_map, path):
     for key in path:
         nested_map = nested_map[key]
     return nested_map
+
+def get_json(url):
+    response = requests.get(url)
+    return response.json()
