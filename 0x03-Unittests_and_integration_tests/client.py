592e50fdefeccc17c512c4c09fdbfd651da12a64
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-29 06:58:55 +0300

    Parameterize and patch as decorators

---
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Client module to interface with GitHub API."""
+import requests
+
+
+def get_json(url):
+    """GET JSON from a given URL."""
+    response = requests.get(url)
+    return response.json()
+
+
+class GithubOrgClient:
+    """GitHub Organization client."""
+
+    ORG_URL = "https://api.github.com/orgs/{}"
+
+    def __init__(self, org_name):
+        self.org_name = org_name
+
+    @property
+    def org(self):
+        """Get organization information."""
+        return get_json(self.ORG_URL.format(self.org_name))
