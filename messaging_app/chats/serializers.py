723b61cccf83037bc493b662973c2098e67a08a1
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-30 11:08:10 +0300

    Checks for nested relationships are handled properly, like including messages within a conversation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,6 +3,7 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    extra_note = serializers.CharField(required=False, default="")
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
@@ -10,6 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    formatted_sent_at = serializers.SerializerMethodField()
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
@@ -21,3 +23,7 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+    def validate(self, data):
+        if 'participants'in data and len(data['participants']) < 2:
+            raise serializers.ValidationError("A conversation must include at least two participants.")
+        return data
