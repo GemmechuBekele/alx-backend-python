b768ff7664a34f5c6659677e7e4b07c3dc0c8cb5
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-05-30 18:38:28 +0300

    Build api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import  Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing conversations.
+    Provides CRUD operations for Conversation model.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        participants = request.data.get('participants', [])
+        if len(participants) < 2:
+            return Response(
+                {"error": "A conversation must include at least two participants."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participants)
+        conversation.save()
+        
+        serializer = self.get_serializer(conversation)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+        
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing messages.
+    Provides CRUD operations for Message model.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """
+        Override to set the sender automatically to the current user.
+        """
+        serializer.save(sender=self.request.user)
